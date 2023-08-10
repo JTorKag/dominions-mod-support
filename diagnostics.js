e0b6c7c4c6a896329399b8c59637459d66feeaff
JTorKag <jimmy.e.torres@gmail.com>
2023-08-10 00:25:45 -0700

    added quickfixes and additional error checking

--- diagnostics.js
+++ diagnostics.js
@@ -19,24 +19,37 @@ class ErrorDiagnosticProvider {
 
     checkMissingEnd(lines, diagnostics, startValues) {
         let lastCommand = null;
-    
+        
         for (let i = 0; i < lines.length; i++) {
             const line = lines[i].trim();
-    
+            
             if (line.includes('#end')) {
                 lastCommand = null;
             }
-    
+            
             for (const command of startValues.command) {
                 if (line.startsWith(command)) {
                     if (lastCommand && !line.endsWith('#end')) {
                         const range = new vscode.Range(new vscode.Position(i, 0), new vscode.Position(i, line.length));
                         const diagnostic = new vscode.Diagnostic(range, `Missing #end command for ${lastCommand} above this.`, vscode.DiagnosticSeverity.Error);
+                        diagnostic.code = 'missing-end-above';
                         diagnostics.push(diagnostic);
                     }
                     lastCommand = command;
                     break; // No need to continue checking after a match
                 }
+            }
+        }
+        
+        // Check if there's a missing #end by the end of the document after a startValue
+        if (lastCommand) {
+            const lastLineIndex = lines.length - 1;
+            const lastLine = lines[lastLineIndex].trim();
+            if (!lastLine.endsWith('#end')) {
+                const range = new vscode.Range(new vscode.Position(lastLineIndex, 0), new vscode.Position(lastLineIndex, lastLine.length));
+                const diagnostic = new vscode.Diagnostic(range, `Missing #end command for ${lastCommand} at the end of the document.`, vscode.DiagnosticSeverity.Error);
+                diagnostic.code = 'missing-end-below';
+                diagnostics.push(diagnostic);
             }
         }
     }
@@ -45,17 +58,68 @@ class ErrorDiagnosticProvider {
     
 
     checkFloatValues(lines, diagnostics) {
+        const exclusionCommands = ['#color', '#maptextcol', '#secondarycolor', '#version'];
+        
         for (let i = 0; i < lines.length; i++) {
-            const words = lines[i].split(/\s+/);
+            const line = lines[i].trim();
+            
+            // Check if the line starts with any of the exclusion commands
+            if (exclusionCommands.some(command => line.startsWith(command))) {
+                continue;
+            }
+            
+            const words = line.split(/\s+/);
             for (const word of words) {
-                if (!isNaN(parseFloat(word))) {
-                    const range = new vscode.Range(new vscode.Position(i, lines[i].indexOf(word)), new vscode.Position(i, lines[i].indexOf(word) + word.length));
-                    const diagnostic = new vscode.Diagnostic(range, 'Float value found', vscode.DiagnosticSeverity.Error);
+                // Check if the word contains a period (.) indicating a potential floating-point number
+                if (/\d+\./.test(word)) {
+                    const range = new vscode.Range(
+                        new vscode.Position(i, line.indexOf(word)),
+                        new vscode.Position(i, line.indexOf(word) + word.length)
+                    );
+                    const diagnostic = new vscode.Diagnostic(
+                        range,
+                        'Floating-point value found',
+                        vscode.DiagnosticSeverity.Error
+                    );
                     diagnostics.push(diagnostic);
                 }
             }
         }
     }
+
+    checkColorValues(lines, diagnostics) {
+        const colorCommands = ['#color', '#maptextcol', '#secondarycolor'];
+    
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i].trim();
+    
+            for (const command of colorCommands) {
+                if (line.startsWith(command)) {
+                    const valueMatch = line.match(/(\d+\.\d+)/);
+                    if (valueMatch) {
+                        const value = parseFloat(valueMatch[0]);
+                        if (value < 0.0 || value > 1.0) {
+                            const range = new vscode.Range(
+                                new vscode.Position(i, line.indexOf(valueMatch[0])),
+                                new vscode.Position(i, line.indexOf(valueMatch[0]) + valueMatch[0].length)
+                            );
+                            const diagnostic = new vscode.Diagnostic(
+                                range,
+                                `Value for ${command} should be between 0.0 and 1.0`,
+                                vscode.DiagnosticSeverity.Error
+                            );
+                            diagnostics.push(diagnostic);
+                        }
+                    }
+                    break; // No need to continue checking after a match
+                }
+            }
+        }
+    }
+    
+    
+    
+    
 
     async analyzeDocument(document, diagnosticCollection, startValues) {
         const diagnostics = [];
@@ -64,23 +128,66 @@ class ErrorDiagnosticProvider {
 
         this.checkMissingEnd(lines, diagnostics, startValues);
         this.checkFloatValues(lines, diagnostics);
+        this.checkColorValues(lines,diagnostics);
 
         diagnosticCollection.set(document.uri, diagnostics);
     }
 
     async activate(context) {
         console.log('Error Diagnostic Provider active');
-
+    
         this.needEndJson = await this.loadJson(context.asAbsolutePath('/json/needEnd.json'));
-
+    
         const diagnosticCollection = vscode.languages.createDiagnosticCollection('dominionsmod');
-
+    
         vscode.workspace.onDidOpenTextDocument(document => this.analyzeDocument(document, diagnosticCollection, this.needEndJson), this, context.subscriptions);
         vscode.workspace.onDidChangeTextDocument(event => this.analyzeDocument(event.document, diagnosticCollection, this.needEndJson), this, context.subscriptions);
         vscode.workspace.onDidCloseTextDocument(document => diagnosticCollection.delete(document.uri), null, context.subscriptions);
+    
+        const codeActionProvider = vscode.languages.registerCodeActionsProvider(
+            'dominionsmod', // Language ID
+            {
+                provideCodeActions: (document, rangeOrSelection, context, token) => {
+                    const fixes = [];
 
-        context.subscriptions.push(diagnosticCollection);
+                    // Check if the diagnostic has the missing #end warning
+                    for (const diagnostic of context.diagnostics) {
+                        if (diagnostic.code === 'missing-end-above' && diagnostic.range.contains(rangeOrSelection.start)) {
+                            const fix = new vscode.CodeAction('Add #end command above', vscode.CodeActionKind.QuickFix);
+                            fix.edit = new vscode.WorkspaceEdit();
+
+                            const lineToAdd = diagnostic.range.start.line;
+                            const newText = '#end\n';
+
+                            fix.edit.insert(document.uri, new vscode.Position(lineToAdd, 0), newText);
+
+                            fix.isPreferred = true;
+                            fix.diagnostics = [diagnostic];
+                            fixes.push(fix);
+                        } else if (diagnostic.code === 'missing-end-below' && diagnostic.range.isEqual(rangeOrSelection)) {
+                            const fix = new vscode.CodeAction('Add #end command at the end of the document', vscode.CodeActionKind.QuickFix);
+                            fix.edit = new vscode.WorkspaceEdit();
+
+                            const lineToAdd = document.lineCount; // Insert at the end of the document
+                            const newText = '\n#end\n';
+
+                            fix.edit.insert(document.uri, new vscode.Position(lineToAdd, 0), newText);
+
+                            fix.isPreferred = true;
+                            fix.diagnostics = [diagnostic];
+                            fixes.push(fix);
+                        }
+                    }
+
+                    return fixes;
+                }
+            }
+        );
+    
+        context.subscriptions.push(diagnosticCollection, codeActionProvider);
     }
+    
+    
 
     dispose() {
         // Dispose of resources if needed
